b7754dce2830d148299ad5ac74fcc519400d9fed
LearnedDr <fredrickj.chang@gmail.com>
2023-04-25 22:12:10 -0400

    did some schemas and resolvers, fixing authMiddlerware in server.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -10,32 +10,50 @@ const resolvers = {
         },
         // show specific user with attached book schema
         user: async (parent, { username }) => {
-            return User.findOne ({ username });
+            return User.findOne({ username });
         },
     },
     Mutation: {
         addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
+            const newUser = await User.create({ username, email, password });
             const token = signToken(newUser);
             return { token, newUser };
-          },
+        },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('No user found with this email address');
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          },
-    }
-}
 
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addBook: async (parent, { authors, description, bookId, image, link, title }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    {
+                        $addToSet: {
+                            savedBooks: { authors, description, bookId, image, link, title }
+                        },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+
+module.exports = resolvers;
